8070ab91308e4db5df82ec3d1878fc567321f83b
Nick_theGeek <Nick_theGeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 13:30:51 +0000

    Initial Commit to WP plugin repo SVN
    git-svn-id: https://plugins.svn.wordpress.org/genesis-author-pro/trunk@1136191 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/class.Genesis_Author_Pro_Widget.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Genesis_Author_Pro_Widget class.
+ * Generates the Genesis Author Pro Featured Book Widget.
+ */
+class Genesis_Author_Pro_Widget extends WP_Widget {
+
+	/**
+	 * Holds widget settings defaults, populated in constructor.
+	 *
+	 * @var array
+	 */
+	protected $defaults;
+
+	/**
+	 * Constructor. Set the default widget options and create widget.
+	 *
+	 * @since 0.1.8
+	 */
+	function __construct() {
+
+		$this->defaults = array(
+			'title'              => '',
+			'show_author'        => '',
+			'book_id'            => '',
+			'show_image'         => 0,
+			'image_alignment'    => '',
+			'image_size'         => 'author-pro-image',
+			'show_featured_text' => '',
+			'show_title'         => 0,
+			'show_content'       => 0,
+			'content_limit'      => '',
+			'show_price'         => '',
+			'more_text'          => __( 'View Book', 'genesis-author-pro' ),
+		);
+
+		$widget_ops = array(
+			'classname'   => 'featured-content featuredpage featuredbook',
+			'description' => __( 'Displays featured page with thumbnails', 'genesis-author-pro' ),
+		);
+
+		$control_ops = array(
+			'id_base' => 'featured-book',
+			'width'   => 200,
+			'height'  => 250,
+		);
+
+		parent::__construct( 'featured-book', __( 'Genesis Author Pro - Featured Book', 'genesis-author-pro' ), $widget_ops, $control_ops );
+		
+		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_scripts') );
+
+	}
+	
+	/**
+	 * Action added on the wp_enqueue_scripts hook.
+	 * Loads the author stylesheet if the widget is active and the default styles are not being overridden.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	function enqueue_scripts(){
+	
+		if( apply_filters( 'genesis_author_pro_load_default_styles', true ) && is_active_widget( false, false, $this->id_base, true ) ) {
+			
+			wp_enqueue_style( 'genesis_author_pro',
+				GENESIS_AUTHOR_PRO_RESOURCES_URL . 'css/default.css',
+				false,
+				'1.0.0'
+			);
+			
+		}           
+	
+	}
+
+	/**
+	 * Echo the widget content.
+	 *
+	 * @since 0.1.8
+	 *
+	 * @global WP_Query $wp_query Query object.
+	 * @global integer  $more
+	 *
+	 * @param array $args Display arguments including before_title, after_title, before_widget, and after_widget.
+	 * @param array $instance The settings for the particular instance of the widget
+	 */
+	function widget( $args, $instance ) {
+
+		//* Merge with defaults
+		$instance = wp_parse_args( (array) $instance, $this->defaults );
+
+		new Genesis_Author_Pro_Widget_Output( $args, $instance, $this );
+
+	}
+
+	/**
+	 * Update a particular instance.
+	 *
+	 * This function should check that $new_instance is set correctly.
+	 * The newly calculated value of $instance should be returned.
+	 * If "false" is returned, the instance won't be saved/updated.
+	 *
+	 * @since 0.1.8
+	 *
+	 * @param array $new_instance New settings for this instance as input by the user via form()
+	 * @param array $old_instance Old settings for this instance
+	 * @return array Settings to save or bool false to cancel saving
+	 */
+	function update( $new_instance, $old_instance ) {
+
+		$new_instance['title']     = strip_tags( $new_instance['title'] );
+		$new_instance['more_text'] = strip_tags( $new_instance['more_text'] );
+		return $new_instance;
+
+	}
+
+	/**
+	 * Echo the settings update form.
+	 *
+	 * @since 0.1.8
+	 *
+	 * @param array $instance Current settings
+	 */
+	function form( $instance ) {
+
+		//* Merge with defaults
+		$instance = wp_parse_args( (array) $instance, $this->defaults );
+
+		new Genesis_Author_Pro_Widget_Admin( $instance, $this );
+
+	}
+
+}
---
+++ classes/class.Genesis_Author_Pro_Widget_Output.php
@@ -0,0 +1,198 @@
+<?php
+
+/**
+ * Genesis_Author_Pro_Widget class.
+ * Generates the Genesis Author Pro Featured Book Widget.
+ */
+class Genesis_Author_Pro_Widget_Output {
+
+	private $_args;
+
+	private $_instance;
+
+	private $_widget_object;
+
+	/**
+	 * Constructor. Set the default widget options and create widget.
+	 *
+	 * @since 0.1.8
+	 */
+	function __construct( $args, $instance, $widget_object ) {
+
+		$this->_args          = $args;
+		$this->_instance      = $instance;
+		$this->_widget_object = $widget_object;
+
+		require_once( GENESIS_AUTHOR_PRO_FUNCTIONS_DIR . 'template.php' );
+
+		$this->widget_output();
+
+	}
+
+	/**
+	 * Echo the widget content.
+	 *
+	 * @since 0.1.8
+	 *
+	 * @global WP_Query $wp_query Query object.
+	 * @global integer  $more
+	 */
+	function widget_output() {
+
+		global $wp_query, $Genesis_Author_Pro_CPT;
+
+		echo $this->_args['before_widget'];
+
+		//* Set up the author bio
+		if ( ! empty( $this->_instance['title'] ) )
+			echo $this->_args['before_title'] . apply_filters( 'widget_title', $this->_instance['title'], $this->_instance, $this->_widget_object->id_base ) . $this->_args['after_title'];
+
+		$wp_query = new WP_Query( array( 'post__in' => array( $this->_instance['book_id'] ), 'post_type' => $Genesis_Author_Pro_CPT->post_type ) );
+
+		if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+			genesis_markup( array(
+					'html5'   => '<article %s>',
+					'xhtml'   => sprintf( '<div class="%s">', implode( ' ', get_post_class() ) ),
+					'context' => 'entry',
+				) );
+
+		$image = genesis_get_image( array(
+				'format'  => 'html',
+				'size'    => $this->_instance['image_size'],
+				'context' => 'featured-page-widget',
+				'attr'    => genesis_parse_attr( 'entry-image-widget' ),
+			) );
+
+		if ( $this->_instance['show_image'] && $image ) {
+
+			$banner = ( $this->_instance['show_featured_text'] && $text = genesis_author_pro_get_book_meta( 'featured_text' ) ) ? sprintf( '<div class="book-featured-text-banner">%s</div>', $text ) : '';
+
+			printf( '<div class="author-pro-featured-image image-%s"><a class="%s" href="%s" title="%s">%s %s</a></div>', esc_attr( $this->_instance['image_alignment'] ), esc_attr( $this->_instance['image_alignment'] ), get_permalink(), the_title_attribute( 'echo=0' ), $image, $banner );
+
+		}
+
+		if( ! empty( $this->_instance['show_title'] ) || ! empty( $this->_instance['show_author'] ) ){
+
+			echo genesis_html5() ? '<header class="entry-header">' : '';
+
+			if ( ! empty( $this->_instance['show_title'] ) ) {
+
+				$title = get_the_title() ? get_the_title() : __( '(no title)', 'genesis' );
+
+				/**
+				 * Filter the featured book title.
+				 *
+				 *
+				 * @param string $title    Featured book title.
+				 * @param array  $this->_instance {
+				 *     Widget settings for this instance.
+				 *
+				 *     @type string $title           Widget title.
+				 *     @type int    $book_id         ID of the featured page.
+				 *     @type bool   $show_image      True if featured image should be shown, false
+				 *                                   otherwise.
+				 *     @type string $image_alignment Image alignment: alignnone, alignleft,
+				 *                                   aligncenter or alignright.
+				 *     @type string $image_size      Name of the image size.
+				 *     @type bool   $show_title      True if featured page title should be shown,
+				 *                                   false otherwise.
+				 *     @type bool   $show_content    True if featured page content should be shown,
+				 *                                   false otherwise.
+				 *     @type int    $content_limit   Amount of content to show, in characters.
+				 *     @type int    $more_text       Text to use for More link.
+				 * }
+				 * @param array  $this->_args     {
+				 *     Widget display arguments.
+				 *
+				 *     @type string $before_widget Markup or content to display before the widget.
+				 *     @type string $before_title  Markup or content to display before the widget title.
+				 *     @type string $after_title   Markup or content to display after the widget title.
+				 *     @type string $after_widget  Markup or content to display after the widget.
+				 * }
+				 */
+				$title = apply_filters( 'genesis_author_pro_featured_book_title', $title, $this->_instance, $this->_args );
+
+				printf( '<h2 class="entry-title"><a href="%s">%s</a></h2>', get_permalink(), $title );
+
+			}
+
+			//include the author details if selected
+			$this->_instance['show_author'] ? genesis_author_pro_do_by_line() : '';
+
+			echo genesis_html5() ? '</header>' : '';
+
+		}
+
+		//show the content, content limit, or excerpt as selected
+		if ( ! empty( $this->_instance['show_content'] ) || ! empty( $this->_instance['show_price'] ) || ! empty( $this->_instance['more_text'] ) ) {
+
+			echo genesis_html5() ? '<div class="entry-content">' : '';
+
+			if ( ! empty( $this->_instance['show_content'] ) ) {
+
+				if ( 'excerpt' == $this->_instance['show_content'] ) {
+					the_excerpt();
+				}
+				elseif ( 'content-limit' == $this->_instance['show_content'] ) {
+
+					add_filter( 'get_the_content_limit', array( $this, 'content_limit_filter' ) );
+
+					the_content_limit( (int) $this->_instance['content_limit'], '' );
+
+					remove_filter( 'get_the_content_limit', array( $this, 'content_limit_filter' ) );
+
+				}
+				else {
+
+					global $more;
+
+					$orig_more = $more;
+					$more = 0;
+
+					the_content( '' );
+
+					$more = $orig_more;
+
+				}
+
+			}
+
+			echo empty( $this->_instance['show_price'] ) ? '' : sprintf( '<p>%s</p>', genesis_author_pro_get_price() );
+
+			//show the link to view the single book page if selected
+			echo empty( $this->_instance['more_text'] )  ? '' : sprintf( '<p><a href="%s" class="button">%s</a></p>', get_permalink(), $this->_instance['more_text'] );
+
+			echo genesis_html5() ? '</div>' : '';
+
+		}
+
+		genesis_markup( array(
+				'html5' => '</article>',
+				'xhtml' => '</div>',
+			) );
+
+		endwhile;
+		endif;
+
+		//* Restore original query
+		wp_reset_query();
+
+		echo $this->_args['after_widget'];
+
+	}
+
+	/**
+	 * Filter on get_the_content_limit.
+	 * Adds an ellipse inside the <p></p> of the returned text.
+	 *
+	 * @access public
+	 * @static
+	 * @param string $text
+	 * @return string
+	 */
+	static public function content_limit_filter( $text ){
+		return str_replace( '</p>', '&#x02026;</p>', $text );
+	}
+
+}
